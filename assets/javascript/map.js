9301bf4fd7fdec94914adf81ed1f670a78473127
MudassirAhmed0 <84226929+MudassirAhmed0@users.noreply.github.com>
2023-05-01 00:40:14 +0500

    origin

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,20 +1,15 @@
-
 function myMap() {
-    var mapProp = {  
-         center: { lat:21.5533311, lng: 39.1716305 },  
-        //  center: ( ),  
-        zoom: 14,
-        draggable: true
-    };
+  var mapProp = {
+    center: { lat: 21.551913047921776, lng: 39.17393776073538 },
+    //  center: ( ),
+    zoom: 14,
+    draggable: true,
+  };
 
- 
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
 
-   
- 
-    marker = new google.maps.Marker({
-        position: new google.maps.LatLng(21.5533311,39.1716305) ,
-        map: map,
-    });
+  marker = new google.maps.Marker({
+    position: new google.maps.LatLng(21.551913047921776, 39.17393776073538),
+    map: map,
+  });
 }
-   
