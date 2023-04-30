3f791f755652b41b0061b4034540928b773fde85
MudassirAhmed0 <84226929+MudassirAhmed0@users.noreply.github.com>
2023-04-30 13:17:40 +0500

    origin

--- assets/javascript/homeSwipers.js
+++ assets/javascript/homeSwipers.js
@@ -1,20 +1,20 @@
-// const sectorGap = (window.innerWidth / 100) * 3.38541666667;
-// const sectorsSwiper = new Swiper(".sectorsSwiper", {
-//   loop: true,
-//   slidesPerView: window.innerWidth > 768 ? 2.4 : 1.4,
+const sectorGap = (window.innerWidth / 100) * 3.38541666667;
+const sectorsSwiper = new Swiper(".sectorssSwiper", {
+  loop: true,
+  slidesPerView: window.innerWidth > 768 ? 2.4 : 1.4,
 
-//   spaceBetween: sectorGap,
-//   navigation: {
-//     nextEl: ".swiper-button-next",
-//     prevEl: ".swiper-button-prev",
-//   },
-// });
-// setTimeout(function () {
-//   sectorsSwiper.update(true);
-//   sectorsSwiper.slideTo(50, 0);
-//   sectorsSwiper.slideTo(0, 0);
-//   sectorsSwiper.update(true);
-// }, 500);
+  spaceBetween: sectorGap,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+setTimeout(function () {
+  sectorsSwiper.update(true);
+  sectorsSwiper.slideTo(50, 0);
+  sectorsSwiper.slideTo(0, 0);
+  sectorsSwiper.update(true);
+}, 500);
 
 const testiomonialSwiperGap = (window.innerWidth / 100) * 2.34375;
 const testiomonialSwiper = new Swiper(".testiomonialSwiper", {
